e142c852143dfbaaf3069dfa878b7af36665f307
paigerackley <102173123+paigerackley@users.noreply.github.com>
2022-06-27 11:16:05 -0500

    no

--- acquire.py
+++ acquire.py
@@ -1,59 +1,95 @@
+'''
+Module to encapsulate data acquisition.
+Primary functions to use are
+- get_store_item_demand_data()
+- get_opsd_data()
+If you want to get the individual datasets from the store item demand data you
+can do so with
+- get_items_data
+- get_stores_dat
+- get_sales_data
+'''
+import os
+import requests
 import pandas as pd
 
-import requests
+def get_store_data_from_api():
+    response = requests.get('https://api.data.codeup.com/api/v1/stores')
+    data = response.json()
+    stores = pd.DataFrame(data['payload']['stores'])
+    stores = pd.DataFrame(stores)
+    return stores
 
+def get_items_data_from_api():
+    domain = 'https://api.data.codeup.com'
+    endpoint = '/api/v1/items'
+    items = []
+    while True:
+        url = domain + endpoint
+        response = requests.get(url)
+        data = response.json()
+        print(f'\rGetting page {data["payload"]["page"]} of {data["payload"]["max_page"]}: {url}', end='')
+        items.extend(data['payload']['items'])
+        endpoint = data['payload']['next_page']
+        if endpoint is None:
+            break
+    items = pd.DataFrame(items)
+    return items
 
-# Setting up for function
-endpoint = 'sales'
-host = 'https://python.zgulde.net/'
-api = 'api/v1/'
-
-url = host + api + endpoint
-
-response = requests.get(url)
-
-
-# Dry Out Function (Dont Repeat Yourself)
-# We often build functions once we know that the code we want is 
-# reusable and not bespoke
-# function that takes in the string "sales", "items", or "stores"
-
-def get_all(endpoint):
-    """ Read all records on all pages """
-    
-    if endpoint not in ["sales", "items", "stores"]:
-        return "Not available from this API. Check the documentation"
-    
-    host = "https://python.zgulde.net/"
-    api = "api/v1/"
-
-    url = host + api + endpoint
-
+def get_sales_data_from_api():
+    base_url = 'https://api.data.codeup.com/api/v1/sales?page='
+    sales = []
+    url = base_url + str(1)
     response = requests.get(url)
+    data = response.json()
+    max_page = data['payload']['max_page']
+    sales.extend(data['payload']['sales'])
+    page_range = range(2, max_page + 1)
 
-    if response.ok:
-        payload = response.json()["payload"]
+    for page in page_range:
+        url = base_url + str(page)
+        print(f'\rFetching page {page}/{max_page} {url}', end='')
+        response = requests.get(url)
+        data = response.json()
+        sales.extend(data['payload']['sales'])
+    sales = pd.DataFrame(sales)
+    return sales
 
-        # endpoint should be "items", "sales", or "stores"
-        contents = payload[endpoint]
+def get_stores_data():
+    if os.path.exists('stores.csv'):
+        return pd.read_csv('stores.csv')
+    df = get_store_data_from_api()
+    df.to_csv('stores.csv', index=False)
+    return df
 
-        # Make a dataframe of the contents
-        df = pd.DataFrame(contents)
+def get_items_data():
+    if os.path.exists('items.csv'):
+        return pd.read_csv('items.csv')
+    df = get_items_data_from_api()
+    df.to_csv('items.csv', index=False)
+    return df
 
-        next_page = payload["next_page"]
+def get_sales_data():
+    if os.path.exists('sales.csv'):
+        return pd.read_csv('sales.csv')
+    df = get_sales_data_from_api()
+    df.to_csv('sales.csv', index=False)
+    return df
 
-        while next_page:
-            # Append the next_page url piece
-            url = host + next_page
-            response = requests.get(url)
+def get_store_item_demand_data():
+    sales = get_sales_data()
+    stores = get_stores_data()
+    items = get_items_data()
 
-            payload = response.json()["payload"]
+    sales = sales.rename(columns={'store': 'store_id', 'item': 'item_id'})
+    df = pd.merge(sales, stores, how='inner', left_on='store_id', right_on='store_id')
+    df = pd.merge(df, items, how='inner', left_on='item_id', right_on='item_id')
 
-            next_page = payload["next_page"]    
-            contents = payload[endpoint]
+    return df
 
-            df = pd.concat([df, pd.DataFrame(contents)])
-
-            df = df.reset_index(drop=True)
-
+def get_opsd_data():
+    if os.path.exists('opsd.csv'):
+        return pd.read_csv('opsd.csv')
+    df = pd.read_csv('https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv')
+    df.to_csv('opsd.csv', index=False)
     return df
